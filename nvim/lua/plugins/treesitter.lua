6ad98c6d7431cb06bb52d17eaa2d63c88c63fd36
Néstor Benitez <nestormbg1011@gmail.com>
2024-03-13 21:02:47 -0300

    some configs have been made

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
-return { 
+return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
+  config = function() 
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript"},
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
       highlight = { enable = true },
       indent = { enable = true },
     })
   end
 }
-
